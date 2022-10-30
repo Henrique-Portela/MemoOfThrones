c83fd71782e2ea5ef811c7b9cd6f450af3645ae0
Henrique Portela <henriquepsoares@gmail.com>
2022-10-30 00:31:38 -0300

    Game.js-Grid

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 const cardsGrid = document.querySelector('.cardsgrid')
-const cards = ['Arya', 'Brienne', 'Cersei', 'Daenerys', 'Grey-Worm', 'Jon', 'Missandei', 'Ned', 'nitgh-king', 'Samwell', 'Tormund', 'Tyrion']
+const characterArr = ['Arya', 'Brienne', 'Cersei', 'Daenerys', 'Grey-Worm', 'Jon', 'Missandei', 'Ned', 'nitgh-king', 'Samwell', 'Tormund', 'Tyrion']
 
 
 function createElement(tag, className) {
@@ -8,18 +8,57 @@ function createElement(tag, className) {
     return element;
 }
 
-function createCard() {
+function createCard(character) {
     const card = createElement('div', 'cards')
     const frontCard = createElement('div', 'face frontcard');
     const backCard = createElement('div', 'face backcard');
 
+    frontCard.style.backgroundImage = `url('../Images/${character}.jpeg')`
+
     card.appendChild(frontCard);
     card.appendChild(backCard);
+    card.addEventListener('click', cardReveal)
     
     return card
+
 }
 
 function createGame() {
-    
+
+    const doubleCharacter = [...characterArr, ...characterArr]
+    const randomCharacter = doubleCharacter.sort(function(a, b) {
+        return 0.5 - Math.random()
+    })
+
+    for(let i=0; i<randomCharacter.length; i++){
+   
+    const cardGame = createCard(randomCharacter[i]);
+    cardsGrid.appendChild(cardGame)
+    }
 }
+
+let cardOne = ''
+let cardTwo = ''
+
+const cardReveal = ({ target }) => {
+    if(target.parentNode.className.includes('card-reveal')) {
+        return;
+    }
+
+    if (cardOne === '') {
+        target.parentNode.classList.add('card-reveal');
+        cardOne = target.parentNode;
+    } else if (cardTwo === '') {
+        target.parentNode.classList.add('card-reveal');
+        cardTwo = target.parentNode;
+    }
+    
+   
+}
+
+createGame()
+
+
+
+
 
